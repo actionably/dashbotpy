293b27f8e68c398e9530100bbb5c573bf5cf3ecf
Timothy Cloninger <timothy.cloninger@life.church>
2018-09-13 20:45:03 -0500

    Deserialize SDK objects to prep for requests

--- dashbot/alexa.py
+++ dashbot/alexa.py
@@ -5,11 +5,13 @@ from .generic import Generic
 
 class Alexa(Generic):
     def _load_data(**kwargs):
+        event = kwargs['request_envelope'].to_dict()
+        event['request']['timestamp'] = event['request']['timestamp'].isoformat()
         data = {
             'dashbot_timestamp': int(time.time() * 1000),
-            'event': kwargs['event'],
+            'event': event,
         }
         if 'response' in kwargs:
-            data['response'] = kwargs['response']
+            data['response'] = kwargs['response'].to_dict()
 
         return data
